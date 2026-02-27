20ae4876ed9751a5cb72e89dd4346db659dc7401
Vijaya-Mohan <vijayakalaiselvan236@gmail.com>
2026-02-27 15:45:42 +0000

    Result Check added in the program

---
+++ Stage2_ResultCheck.py
@@ -0,0 +1,31 @@
+
+def calculator(num1, num2, ops):
+  if ops == "+":
+    return num1 + num2
+  elif ops == "-":
+    return num1 - num2
+  elif ops == "*":
+    return num1 * num2
+
+  elif ops == "/":
+    return num1 / num2
+  else:
+    return "Invalid operation"
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+ops = input("Enter which operation need to perform?")
+
+
+
+result = calculator(num1, num2, ops)
+print("Result =",result)
+
+def result_chk(result):
+  if result == 0:
+    return "Zero"
+  elif result > 0:
+      return "Positive"
+  else:
+    return "Negative"
+print("Result is", result_chk(result))
